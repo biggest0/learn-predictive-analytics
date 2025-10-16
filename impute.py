cc5a75153ae10675d258dc63557614c370850cc4
biggest0 <thebiggestkai@gmail.com>
2025-10-15 21:57:35 -0700

    add basic impute function for missing col variables

---
+++ impute.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy  as np
+from sklearn.model_selection import train_test_split
+import statsmodels.api       as     sm
+from   sklearn               import metrics
+
+
+def convertNAcellsToNum(colName, df, measureType):
+    # Create two new column names based on original column name.
+    indicatorColName = 'm_'   + colName # Tracks whether imputed.
+    imputedColName   = 'imp_' + colName # Stores original & imputed data.
+
+    # Get mean or median depending on preference.
+    imputedValue = 0
+    if(measureType=="median"):
+        imputedValue = df[colName].median()
+    elif(measureType=="mode"):
+        imputedValue = float(df[colName].mode())
+    else:
+        imputedValue = df[colName].mean()
+
+    # Populate new columns with data.
+    imputedColumn  = []
+    indictorColumn = []
+    for i in range(len(df)):
+        isImputed = False
+
+        # mi_OriginalName column stores imputed & original data.
+        if(np.isnan(df.loc[i][colName])):
+            isImputed = True
+            imputedColumn.append(imputedValue)
+        else:
+            imputedColumn.append(df.loc[i][colName])
+
+        # mi_OriginalName column tracks if is imputed (1) or not (0).
+        if(isImputed):
+            indictorColumn.append(1)
+        else:
+            indictorColumn.append(0)
+
+    # Append new columns to dataframe but always keep original column.
+    df[indicatorColName] = indictorColumn
+    df[imputedColName]   = imputedColumn
+    return df
